9b2fa093d2dbf16bac1100c43a128bca9fe19e91
mm10118 <mm10118@ic.ac.uk>
2020-11-12 12:05:35 +0000

    commit 3

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -10,8 +10,46 @@ public class MyServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
-        resp.setContentType("text/html");
-        resp.getWriter().write("Hello, world!");
+        resp.setContentType("html");
+        resp.getWriter().write("<HTML>\n" +
+                "\n" +
+                "<HEAD>\n" +
+                "\n" +
+                "<TITLE>Your Title Here</TITLE>\n" +
+                "\n" +
+                "</HEAD>\n" +
+                "\n" +
+                "<BODY BGCOLOR=\"FFFFFF\">\n" +
+                "\n" +
+                "<CENTER><IMG SRC=\"clouds.jpg\" ALIGN=\"BOTTOM\"> </CENTER>\n" +
+                "\n" +
+                "<HR>\n" +
+                "\n" +
+                "<a href=\"http://somegreatsite.com\">Link Name</a>\n" +
+                "\n" +
+                "is a link to another nifty site\n" +
+                "\n" +
+                "<H1>This is a Header</H1>\n" +
+                "\n" +
+                "<H2>This is a Medium Header</H2>\n" +
+                "\n" +
+                "Send me mail at <a href=\"mailto:support@yourcompany.com\">\n" +
+                "\n" +
+                "support@yourcompany.com</a>.\n" +
+                "\n" +
+                "<P> This is a new paragraph!\n" +
+                "\n" +
+                "<P> <B>This is a new paragraph!</B>\n" +
+                "\n" +
+                "<BR> <B><I>This is a new sentence without a paragraph break, in bold italics.</I></B>\n" +
+                "\n" +
+                "<HR>\n" +
+                "\n" +
+                "</BODY>\n" +
+                "\n" +
+                "</HTML>\n" +
+                "\n" +
+                " ");
         resp.getWriter().write(req.getServletPath());
 
     }
